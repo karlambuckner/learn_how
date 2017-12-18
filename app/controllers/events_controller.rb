111113edbec7656d547ec6319ae01111c620b5f2
Karla Buckner <kmb2662@gmail.com>
2017-12-18 09:43:36 -0800

    add form for adding a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,32 @@
 class EventsController < ApplicationController
+
   def index
     @events = Event.all
     render :index
   end
+
+  def show
+    @event = Event.find(params[:id])
+    render :show
+  end
+
+  def new
+    @event = Event.new
+    render :new
+ end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to  events_path
+    else
+      render :new
+    end
+  end
+
+private
+  def event_params
+    params.require(:event).permit(:name, :location, :price, :description)
+  end
+
 end
