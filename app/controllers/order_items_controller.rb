ee5057fe5037933a4140d63a66013c400a6fe274
Karla Buckner <kmb2662@gmail.com>
2017-12-19 15:39:58 -0800

    amend def for current_order and create of order items and will now work and add items to cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,11 +1,12 @@
 class OrderItemsController < ApplicationController
 
-  def create
-    @order = current_order
-    @order_item = @order.order_items.new
-    @order.save
-    session[:order_id] = @order.id
-  end
+def create
+  @order = current_order
+  @item = @order.order_items.new(item_params)
+  @order.save
+  session[:order_id] = @order.id
+  redirect_to cart_path
+end
 
 def show
   @order = current_order
