39bcfe5b105abf57863ee302244a3da9148b65b5
Karla Buckner <kmb2662@gmail.com>
2017-12-19 09:32:21 -0800

    add order items and cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+    if session[:order_id]
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
+
+  def authorize
+    if !current_user
+      flash[:alert] = "Please sign in to visit this page."
+      redirect_to '/'
+    end
+  end
+
+  def authorize_admin
+    if current_user.admin === true
+      new_product_path
+    else
+      flash[:alert] = "Only an admin can visit this page."
+      redirect_to '/'
+    end
+  end
+
 end
