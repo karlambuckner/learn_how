5cc857349627a060957c816b50e5964336fe41f8
Karla Buckner <kmb2662@gmail.com>
2017-12-19 16:46:25 -0800

    now give a thank you message without error and completely work using stripe payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,7 @@ class ApplicationController < ActionController::Base
     if session[:order_id]
       Order.find(session[:order_id])
     else
-      if current_user
-        if Order.where(:account_id => current_user.id, :status => 'new').any?
-          x = Order.where(:account_id => current_user.id, :status => 'new')
-          Order.find(x.first.id)
-        else
-          Order.new(:account_id => current_user.account.id, :status => 'new')
-        end
-      else
-        Order.new
-      end
+      Order.new
     end
   end
 
